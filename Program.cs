3db48aab2d5bd623e409161716760c4518229b03
MYS0401 <241206@st.yoshida-g.ac.jp>
2025-01-30 13:35:36 +0900

    すべてのカードの値をランダムで代入されるようにする

--- Program.cs
+++ Program.cs
@@ -10,44 +10,34 @@ namespace Pokergame
     {
         static void Main(string[] args)
         {
-            int [] num = new int[4];
+            Random rnd = new Random();
+            int [] card = new int[4];
             int count;
 
             for(; ; )
             {
                 count = 0;
 
-                for (int i = 0; i < 4; i++)
+                for(int i = 0; i < 4; i++)
                 {
-                    for (; ; )
-                    {
-                        Console.Write($"{i + 1}枚目のカードの数値を入力 > ");
-                        bool result = int.TryParse(Console.ReadLine(), out num[i]);
-
-                        if (result == false || num[i] > 13 || num[i] < 1)
-                        {
-                            Console.WriteLine("使用できない値です");
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
+                    card[i] = rnd.Next(1,13); // 1～13までの整数をランダムで代入
                 }
 
-                foreach (int i in num)
+                foreach (int i in card)
                 {
                     Console.Write("{");
                     Console.Write($"{i}");
                     Console.Write("}");
                 }
 
+                Console.WriteLine();
+
                 //役判定
                 for (int i = 0; i < 4; i++)
                 {
                     for (int j = 0; j < 4; j++)
                     {
-                        if (num[i] == num[j])
+                        if (card[i] == card[j])
                         {
                             count++;
                         }
