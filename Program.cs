c527b27e6c73bc0315fcfc243882df76a1ac666d
MYS0401 <241206@st.yoshida-g.ac.jp>
2025-01-30 10:53:28 +0900

    1～4の数字を入力するフォームを4つ用意

--- Program.cs
+++ Program.cs
@@ -10,6 +10,31 @@ namespace Pokergame
     {
         static void Main(string[] args)
         {
+            int num;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (; ; )
+                {
+                    Console.Write($"{i + 1}枚目のカードの数値を入力 > ");
+                    bool result = int.TryParse(Console.ReadLine(), out num);
+
+                    if(result == false || num >4 || num <1)
+                    {
+                        Console.WriteLine("使用できない値です");
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+
+               
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
