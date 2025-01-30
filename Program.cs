231332174f92843a92ccc3c34042c3df4e1c5f9d
MYS0401 <241206@st.yoshida-g.ac.jp>
2025-01-30 13:21:47 +0900

    ゲームを繰り返し遊べるようにする

--- Program.cs
+++ Program.cs
@@ -11,68 +11,91 @@ namespace Pokergame
         static void Main(string[] args)
         {
             int [] num = new int[4];
-            int count = 0;
+            int count;
 
-            for (int i = 0; i < 4; i++)
+            for(; ; )
             {
+                count = 0;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for (; ; )
+                    {
+                        Console.Write($"{i + 1}枚目のカードの数値を入力 > ");
+                        bool result = int.TryParse(Console.ReadLine(), out num[i]);
+
+                        if (result == false || num[i] > 4 || num[i] < 1)
+                        {
+                            Console.WriteLine("使用できない値です");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                foreach (int i in num)
+                {
+                    Console.Write("{");
+                    Console.Write($"{i}");
+                    Console.Write("}");
+                }
+
+                //役判定
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (num[i] == num[j])
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                //Console.WriteLine("{0}",count);
+
+                switch (count)
+                {
+                    case 4:
+                        Console.WriteLine("ノーペア");
+                        break;
+                    case 6:
+                        Console.WriteLine("ワンペア");
+                        break;
+                    case 8:
+                        Console.WriteLine("ツーペア");
+                        break;
+                    case 10:
+                        Console.WriteLine("スリーカード");
+                        break;
+                    case 16:
+                        Console.WriteLine("フォーカード");
+                        break;
+                }
+
                 for (; ; )
                 {
-                    Console.Write($"{i + 1}枚目のカードの数値を入力 > ");
-                    bool result = int.TryParse(Console.ReadLine(), out num[i]);
+                    Console.WriteLine("ゲームを終了しますか 終了: 0 もう一度 :1");
+                    bool result = int.TryParse(Console.ReadLine(), out int date);
 
-                    if (result == false || num[i] > 4 || num[i] < 1) 
+                    if (result == false || date != 1 && date != 0)
                     {
                         Console.WriteLine("使用できない値です");
+                    }
+                    else if(date == 0)
+                    {
+                        return;
                     }
                     else
                     {
                         break;
                     }
                 }
+
             }
 
-            foreach (int i in num)
-            {
-                Console.Write("{");
-                Console.Write($"{i}");
-                Console.Write("}");
-            }
-
-            //役判定
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    if (num[i] == num[j])
-                    {
-                        count++;
-                    }
-                }
-            }
-
-            //Console.WriteLine("{0}",count);
-
-            switch (count)
-            { 
-                case 4:
-                    Console.WriteLine("ノーペア");
-                    break;
-                case 6:
-                    Console.WriteLine("ワンペア");
-                    break;
-                case 8:
-                    Console.WriteLine("ツーペア");
-                    break;
-                case 10:
-                    Console.WriteLine("スリーカード");
-                    break;
-                case 16:
-                    Console.WriteLine("フォーカード");
-                    break;
-            }
-
-
-            Console.ReadLine();
         }
     }
 }
