619d258355de10aac710ce11e57e85c72799414e
MYS0401 <241206@st.yoshida-g.ac.jp>
2025-01-30 12:04:50 +0900

    役を判定するプログラムを用意

--- Program.cs
+++ Program.cs
@@ -10,16 +10,17 @@ namespace Pokergame
     {
         static void Main(string[] args)
         {
-            int num;
+            int [] num = new int[4];
+            int count = 0;
 
             for (int i = 0; i < 4; i++)
             {
                 for (; ; )
                 {
                     Console.Write($"{i + 1}枚目のカードの数値を入力 > ");
-                    bool result = int.TryParse(Console.ReadLine(), out num);
+                    bool result = int.TryParse(Console.ReadLine(), out num[i]);
 
-                    if(result == false || num >4 || num <1)
+                    if (result == false || num[i] > 4 || num[i] < 1) 
                     {
                         Console.WriteLine("使用できない値です");
                     }
@@ -27,12 +28,29 @@ namespace Pokergame
                     {
                         break;
                     }
-
                 }
-
-               
             }
 
+            foreach (int i in num)
+            {
+                Console.Write("{");
+                Console.Write($"{i}");
+                Console.Write("}");
+            }
+
+            //役判定
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (num[i] == num[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            //Console.WriteLine($"{count}");
 
             Console.ReadLine();
         }
